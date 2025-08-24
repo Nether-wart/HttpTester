1c2006e393909da52d50317eb8f419bba788a243
Nether-wart <nether_wart@163.com>
2025-08-24 21:35:06 +0800

    实现多线程发送请求

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,5 +1,11 @@
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.io.InputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -15,6 +21,45 @@ public class Task {
     Map<String,String> headers;
 
 
+    public List<Runnable> getRunnableList(){
+        List<Runnable> runnableList=new ArrayList<>();
+        for (int threads=0;threads<this.threads;threads++){
+            runnableList.add(newRunnable());
+        }
+        return runnableList;
+    }
+
+    private Runnable newRunnable(){
+        return () -> {
+            try(HttpClient client= HttpClient.newBuilder()
+                    .followRedirects(HttpClient.Redirect.NORMAL)
+                    .build()){
+                for (int i=0;i<times;i++){
+                    HttpResponse<InputStream> response=client.send(getRequest(),
+                            HttpResponse.BodyHandlers.ofInputStream());
+                    var in=response.body();
+                    try (in){
+                        byte[] b=new byte[1024];
+                        while (in.read(b)!=-1){}
+                    }
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        };
+    }
+
+    public HttpRequest getRequest(){
+        var builder= HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .method(method, HttpRequest.BodyPublishers.noBody());
+
+        for (Map.Entry<String,String> entry:headers.entrySet()){
+            builder.header(entry.getKey(),entry.getValue());
+        }
+
+        return builder.build();
+    }
 
     public String getName() {
         return name;
